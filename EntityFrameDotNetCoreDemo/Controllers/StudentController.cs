bcf3d4d790be9f1f4766f9c4530150fc22636a3d
anamikasawhney <anamika.sawhney22@gmail.com>
2021-06-03 16:40:25 +0530

    Add project files.

---
+++ EntityFrameDotNetCoreDemo/Controllers/StudentController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameDotNetCoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameDotNetCoreDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDbContext db;
+        public StudentController(StudentDbContext studentDbContext)
+        {
+            db = studentDbContext;
+        }
+        public IActionResult Index()
+        {
+            if(TempData["msg"] != null)
+            ViewBag.msg = TempData["msg"].ToString();
+            return View(db.Students.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            Student student = new Student();
+            return View(student);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (student != null)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                //ViewData["msg"] = "Record Inserted";
+
+                TempData["msg"] = "Record Inserted";
+            }
+            //return View();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Student student = db.Students.FirstOrDefault(x => x.Id == id);
+             if(student!=null)
+                {   return View(student);
+                }
+             else
+            {
+                ViewBag.msg = "Record not found";
+                return View();
+            }
+            }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id, Student student)
+        {
+            db.Students.Remove(student);
+            db.SaveChanges();
+            TempData["msg"] = "Record deleted";
+            return RedirectToAction("Index");
+        
+        }
+
+
+        public IActionResult Display(int id)
+        {
+            Student student = db.Students.FirstOrDefault(x => x.Id == id);
+            if (student != null)
+            {
+                return View(student);
+            }
+            else
+            {
+                ViewBag.msg = "Record not found";
+                return View();
+            }
+        }
+
+
+        public IActionResult Edit(int id)
+    {
+        Student student = db.Students.FirstOrDefault(x => x.Id == id);
+        if (student != null)
+        {
+            return View(student);
+        }
+        else
+        {
+            ViewBag.msg = "Record not found";
+            return View();
+        }
+    }
+
+
+    [HttpPost]
+    public IActionResult Edit(int id, Student student)
+    {
+            foreach(Student temp in db.Students)
+            {
+                if(temp.Id == id)
+                {
+                    temp.Name = student.Name;
+                    temp.Address = student.Address;
+                    temp.Marks = student.Marks;
+                    break;
+                }
+               }
+            db.SaveChanges();
+
+            TempData["msg"] = "Record updated";
+        return RedirectToAction("Index");
+
+    }
+}
+
+
+
+    }
+
+
+
+
+ 
---
+++ EntityFrameDotNetCoreDemo/Models/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameDotNetCoreDemo.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base (options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
